ff7f0024cd91ad937fc90b0252262cc379dc099f
Ferenc Kerekes Jr <ferenckerekes9@gmail.com>
2023-03-06 16:30:37 +0200

    when no history export button disabled #14

--- src/components/settings/ImportAndExport.jsx
+++ src/components/settings/ImportAndExport.jsx
@@ -1,11 +1,15 @@
-import { Typography, Button, Box } from "@mui/material";
+import { Typography, Button, Box, Alert } from "@mui/material";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import { saveAs } from "file-saver";
 
 const ImportAndExport = () => {
+  const searchHistoryJSON = localStorage.getItem("searches");
+  const hasHistory = searchHistoryJSON.length > 2;
+
   const handleExport = () => {
-    const searchHistoryJSON = localStorage.getItem("searches");
-    const blob = new Blob([searchHistoryJSON], { type: "application/json" });
+    const blob = new Blob([searchHistoryJSON], {
+      type: "application/json",
+    });
     saveAs(blob, `search-history-${new Date().toISOString()}.json`);
   };
 
@@ -14,11 +18,16 @@ const ImportAndExport = () => {
       <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
         Import & Export
       </Typography>
-
+      {!hasHistory && (
+        <Alert severity="error" sx={{ my: 2 }}>
+          No Search History!
+        </Alert>
+      )}
       <Button
         variant="contained"
         onClick={handleExport}
         startIcon={<FileDownloadIcon />}
+        disabled={!hasHistory}
       >
         Export
       </Button>
