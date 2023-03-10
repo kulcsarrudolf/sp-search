fcbed123690eb2507ca66d6ecfe262a608f4ddad
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-03-11 00:11:22 +0200

    Color Mode implementation

--- src/components/SpAppBar.jsx
+++ src/components/SpAppBar.jsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 import SettingsButton from "./settings/SettingsButton";
+import SpColorModeButton from "../shared-components/SpColorModeButton";
 
 const SPAppBar = () => (
   <AppBar position="relative">
@@ -9,6 +10,7 @@ const SPAppBar = () => (
       <Typography variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
         SP Search
       </Typography>
+      <SpColorModeButton />
       <SettingsButton />
     </Toolbar>
   </AppBar>
